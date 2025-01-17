df1f219ab53a736b6cbe0a9793e3f447edad7e8c
Nexus <nexus.ext@youbiquo.eu>
2025-01-17 15:03:07 +0100

    update 2

--- apps/antonclk/app.js
+++ apps/antonclk/app.js
@@ -2,12 +2,11 @@ Graphics.prototype.setFontAnton = function(scale) {
   g.setFontCustom(atob("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAf/gAAAAAAAAAAf/gAAAAAAAAAAf/gAAAAAAAAAAf/gAAAAAAAAAAf/gAAAAAAAAAAf/gAAAAAAAAAAf/gAAAAAAAAAAf/gAAAAAAAAAAf/gAAAAAAAAAAf/gAAAAAAAAAAf/gAAAAAAAAAAf/gAAAAAAAAAAf/gAAAAAAAAAAf/gAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAADgAAAAAAAAAAA/gAAAAAAAAAAP/gAAAAAAAAAH//gAAAAAAAAB///gAAAAAAAAf///gAAAAAAAP////gAAAAAAD/////gAAAAAA//////gAAAAAP//////gAAAAH///////gAAAB////////gAAAf////////gAAP/////////gAD//////////AA//////////gAA/////////4AAA////////+AAAA////////gAAAA///////wAAAAA//////8AAAAAA//////AAAAAAA/////gAAAAAAA////4AAAAAAAA///+AAAAAAAAA///gAAAAAAAAA//wAAAAAAAAAA/8AAAAAAAAAAA/AAAAAAAAAAAAgAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAD//////AAAAAB///////8AAAAH////////AAAAf////////wAAA/////////4AAB/////////8AAD/////////+AAH//////////AAP//////////gAP//////////gAP//////////gAf//////////wAf//////////wAf//////////wAf//////////wA//8AAAAAB//4A//wAAAAAAf/4A//gAAAAAAP/4A//gAAAAAAP/4A//gAAAAAAP/4A//wAAAAAAf/4A///////////4Af//////////wAf//////////wAf//////////wAf//////////wAP//////////gAP//////////gAH//////////AAH//////////AAD/////////+AAB/////////8AAA/////////4AAAP////////gAAAD///////+AAAAAf//////4AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAP/gAAAAAAAAAAP/gAAAAAAAAAAf/gAAAAAAAAAAf/gAAAAAAAAAAf/AAAAAAAAAAA//AAAAAAAAAAA/+AAAAAAAAAAB/8AAAAAAAAAAD//////////gAH//////////gAP//////////gA///////////gA///////////gA///////////gA///////////gA///////////gA///////////gA///////////gA///////////gA///////////gA///////////gA///////////gA///////////gAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAH/4AAAAB/gAAD//4AAAAf/gAAP//4AAAB//gAA///4AAAH//gAB///4AAAf//gAD///4AAA///gAH///4AAD///gAP///4AAH///gAP///4AAP///gAf///4AAf///gAf///4AB////gAf///4AD////gA////4AH////gA////4Af////gA////4A/////gA//wAAB/////gA//gAAH/////gA//gAAP/////gA//gAA///8//gA//gAD///w//gA//wA////g//gA////////A//gA///////8A//gA///////4A//gAf//////wA//gAf//////gA//gAf/////+AA//gAP/////8AA//gAP/////4AA//gAH/////gAA//gAD/////AAA//gAB////8AAA//gAA////wAAA//gAAP///AAAA//gAAD//8AAAA//gAAAP+AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAB/+AAAAAD/wAAB//8AAAAP/wAAB///AAAA//wAAB///wAAB//wAAB///4AAD//wAAB///8AAH//wAAB///+AAP//wAAB///+AAP//wAAB////AAf//wAAB////AAf//wAAB////gAf//wAAB////gA///wAAB////gA///wAAB////gA///w//AAf//wA//4A//AAA//wA//gA//AAAf/wA//gB//gAAf/wA//gB//gAAf/wA//gD//wAA//wA//wH//8AB//wA///////////gA///////////gA///////////gA///////////gAf//////////AAf//////////AAP//////////AAP/////////+AAH/////////8AAH///+/////4AAD///+f////wAAA///8P////gAAAf//4H///+AAAAH//gB///wAAAAAP4AAH/8AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAD/wAAAAAAAAAA//wAAAAAAAAAP//wAAAAAAAAB///wAAAAAAAAf///wAAAAAAAH////wAAAAAAA/////wAAAAAAP/////wAAAAAB//////wAAAAAf//////wAAAAH///////wAAAA////////wAAAP////////wAAA///////H/wAAA//////wH/wAAA/////8AH/wAAA/////AAH/wAAA////gAAH/wAAA///4AAAH/wAAA//+AAAAH/wAAA///////////gA///////////gA///////////gA///////////gA///////////gA///////////gA///////////gA///////////gA///////////gA///////////gA///////////gA///////////gA///////////gA///////////gAAAAAAAAH/4AAAAAAAAAAH/wAAAAAAAAAAH/wAAAAAAAAAAH/wAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAB//8AAA/////+B///AAA/////+B///wAA/////+B///4AA/////+B///8AA/////+B///8AA/////+B///+AA/////+B////AA/////+B////AA/////+B////AA/////+B////gA/////+B////gA/////+B////gA/////+A////gA//gP/gAAB//wA//gf/AAAA//wA//gf/AAAAf/wA//g//AAAAf/wA//g//AAAA//wA//g//gAAA//wA//g//+AAP//wA//g////////gA//g////////gA//g////////gA//g////////gA//g////////AA//gf///////AA//gf//////+AA//gP//////+AA//gH//////8AA//gD//////4AA//gB//////wAA//gA//////AAAAAAAH////8AAAAAAAA////AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAD//////gAAAAB///////+AAAAH////////gAAAf////////4AAB/////////8AAD/////////+AAH//////////AAH//////////gAP//////////gAP//////////gAf//////////wAf//////////wAf//////////wAf//////////wAf//////////4A//wAD/4AAf/4A//gAH/wAAP/4A//gAH/wAAP/4A//gAP/wAAP/4A//gAP/4AAf/4A//wAP/+AD//4A///wP//////4Af//4P//////wAf//4P//////wAf//4P//////wAf//4P//////wAP//4P//////gAP//4H//////gAH//4H//////AAH//4D/////+AAD//4D/////8AAB//4B/////4AAA//4A/////wAAAP/4AP////AAAAB/4AD///4AAAAAAAAAH/8AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA//AAAAAAAAAAA//gAAAAAAAAAA//gAAAAAAAAAA//gAAAAAAADgA//gAAAAAAP/gA//gAAAAAH//gA//gAAAAB///gA//gAAAAP///gA//gAAAD////gA//gAAAf////gA//gAAB/////gA//gAAP/////gA//gAB//////gA//gAH//////gA//gA///////gA//gD///////gA//gf///////gA//h////////gA//n////////gA//////////gAA/////////AAAA////////wAAAA///////4AAAAA///////AAAAAA//////4AAAAAA//////AAAAAAA/////4AAAAAAA/////AAAAAAAA////8AAAAAAAA////gAAAAAAAA///+AAAAAAAAA///4AAAAAAAAA///AAAAAAAAAA//4AAAAAAAAAA/+AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAD//gB///wAAAAP//4H///+AAAA///8P////gAAB///+f////4AAD///+/////8AAH/////////+AAH//////////AAP//////////gAP//////////gAf//////////gAf//////////wAf//////////wAf//////////wA///////////wA//4D//wAB//4A//wB//gAA//4A//gA//gAAf/4A//gA//AAAf/4A//gA//gAAf/4A//wB//gAA//4A///P//8AH//4Af//////////wAf//////////wAf//////////wAf//////////wAf//////////gAP//////////gAP//////////AAH//////////AAD/////////+AAD///+/////8AAB///8f////wAAAf//4P////AAAAH//wD///8AAAAA/+AAf//AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAH//gAAAAAAAAB///+AA/+AAAAP////gA//wAAAf////wA//4AAB/////4A//8AAD/////8A//+AAD/////+A///AAH/////+A///AAP//////A///gAP//////A///gAf//////A///wAf//////A///wAf//////A///wAf//////A///wA///////AB//4A//4AD//AAP/4A//gAB//AAP/4A//gAA//AAP/4A//gAA/+AAP/4A//gAB/8AAP/4A//wAB/8AAf/4Af//////////wAf//////////wAf//////////wAf//////////wAf//////////wAP//////////gAP//////////gAH//////////AAH/////////+AAD/////////8AAB/////////4AAAf////////wAAAP////////AAAAB///////4AAAAAD/////wAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAf/AAB/8AAAAAA//AAD/8AAAAAA//AAD/8AAAAAA//AAD/8AAAAAA//AAD/8AAAAAA//AAD/8AAAAAA//AAD/8AAAAAA//AAD/8AAAAAA//AAD/8AAAAAA//AAD/8AAAAAA//AAD/8AAAAAA//AAD/8AAAAAA//AAD/8AAAAAA//AAD/8AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=="), 46, atob("EiAnGicnJycnJycnEw=="), 78 + (scale << 8) + (1 << 16));
 };
 
-
 {
   let drawTimeout;
 
   // Funzione per disegnare l'orologio
-  let draw = function () {
+  let draw = function() {
     var x = g.getWidth() / 2;
     var y = g.getHeight() / 2;
     g.reset().clearRect(Bangle.appRect);
@@ -15,123 +14,275 @@ Graphics.prototype.setFontAnton = function(scale) {
     var timeStr = require("locale").time(date, 1);
     g.setFontAlign(0, 0).setFont("Anton").drawString(timeStr, x, y);
 
-    var dateStr =
-      require("locale").date(date, 0).toUpperCase() +
-      "\n" +
-      require("locale").dow(date, 0).toUpperCase();
-    g.setFontAlign(0, 0).setFont("6x8", 2).drawString(dateStr, x, y + 48);
+    var dateStr = require("locale").date(date, 0).toUpperCase()+"\n"+
+                  require("locale").dow(date, 0).toUpperCase();
+    g.setFontAlign(0, 0).setFont("6x8", 2).drawString(dateStr, x, y+48);
 
     // Ottieni e stampa il MAC address
     var macAddress = NRF.getAddress(); // Ottieni il MAC address
     g.setFontAlign(0, 0).setFont("6x8", 1).drawString("MAC: " + macAddress, x, y + 80);
 
     if (drawTimeout) clearTimeout(drawTimeout);
-    drawTimeout = setTimeout(function () {
+    drawTimeout = setTimeout(function() {
       drawTimeout = undefined;
       draw();
     }, 60000 - (Date.now() % 60000));
   };
 
   Bangle.setUI({
-    mode: "clock",
-    remove: function () {
+    mode : "clock",
+    remove : function() {
       if (drawTimeout) clearTimeout(drawTimeout);
       drawTimeout = undefined;
       delete Graphics.prototype.setFontAnton;
-    },
+    }
   });
 
   Bangle.loadWidgets();
   draw();
-  setTimeout(Bangle.drawWidgets, 0);
+  setTimeout(Bangle.drawWidgets,0);
 
-  // Definizione dei servizi BLE personalizzati
-  const customServices = {
-    0x180D: {
-      0x2A37: {
-        notify: true,
-        readable: true,
-        value: [0x06, 0],
-        onRead: () => [0x06, hrmData && hrmData.confidence >= 50 ? hrmData.bpm : 0],
-      },
-      0x2A38: {
-        value: 0x02,
-        readable: true,
-      },
-    },
-    0x181A: {
-      0x2A6C: {
-        notify: true,
-        readable: true,
-        value: [0, 0, 0],
-        onRead: () => barData ? toByteArray(Math.round(barData.altitude * 100), 3, true) : [0, 0, 0],
-      },
-      0x2A6D: {
-        notify: true,
-        readable: true,
-        value: [0, 0, 0, 0],
-        onRead: () => barData ? toByteArray(Math.round(barData.pressure * 10), 4, false) : [0, 0, 0, 0],
-      },
-    },
-    0x2A77: { // Accelerometer Data (Custom UUID)
-      notify: true,
-      readable: true,
-      value: [0, 0, 0, 0, 0, 0],
-      onRead: () => accelData ? encodeAccel(accelData) : [0, 0, 0, 0, 0, 0],
-    },
-  };
+  // ----------------------------
+  // Funzione per pubblicizzare i servizi BLE
+  // ----------------------------
+  function setupBLEAdvertising() {
+    require("ble_advert").set(0x180d, undefined, {
+      connectable: true,
+      discoverable: true,
+      scannable: true,
+      whenConnected: true,
+    });
 
-  // Funzione per aggiornare i servizi BLE
-  function updateCustomServices() {
+    NRF.setServices({
+      0x180D: { // Heart Rate Service
+        0x2A37: { // Heart Rate Measurement
+          notify: true,
+          readable: true,
+          value: [0x06, 0],
+          onRead: () => [0x06, hrm_1 && hrm_1.confidence >= 50 ? hrm_1.bpm : 0]
+        },
+        0x2A38: { // Sensor Location: Wrist
+          value: 0x02,
+          readable: true
+        }
+      },
+      0x181A: { // Environmental Sensing
+        // Elevation
+        0x2A6C: {
+          notify: true,
+          readable: true,
+          value: [0, 0, 0],
+          onRead: () => bar_1 ? toByteArray(Math.round(bar_1.altitude * 100), 3, true) : [0, 0, 0]
+        },
+        // Pressure
+        0x2A6D: {
+          notify: true,
+          readable: true,
+          value: [0, 0, 0, 0],
+          onRead: () => bar_1 ? toByteArray(Math.round(bar_1.pressure * 10), 4, false) : [0, 0, 0, 0]
+        },
+        // Temperature
+        0x2A1F: {
+          notify: true,
+          readable: true,
+          value: [0, 0],
+          onRead: () => bar_1 ? toByteArray(Math.round(bar_1.temperature * 10), 2, true) : [0, 0]
+        },
+        // Magnetic Flux Density
+        0x2AA1: {
+          notify: true,
+          readable: true,
+          value: [0, 0, 0, 0, 0, 0],
+          onRead: () => mag_1 ? encodeMag(mag_1) : [0, 0, 0, 0, 0, 0]
+        },
+
+        // ----------------------------
+        // AGGIUNTA: Caratteristica personalizzata per l'ACCELEROMETRO
+        // ----------------------------
+        0x2ACC: { // ID fittizio per esempio - Non è uno standard
+          notify: true,
+          readable: true,
+          // Set di byte iniziale (6 byte: x,y,z con 2 byte per ciascuno)
+          value: [0,0, 0,0, 0,0],
+          onRead: () => acc_1 ? encodeAccel(acc_1) : [0,0, 0,0, 0,0]
+        }
+      },
+
+      0x1819: { // Location and Navigation
+        0x2A67: { // Position Quality
+          notify: true,
+          readable: true,
+          value: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+          onRead: () => gps_1 ? encodeGps(gps_1) : [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        }
+      },
+
+      // Nordic UART Service
+      '6e400001-b5a3-f393-e0a9-e50e24dcca9e': {
+         '6e400002-b5a3-f393-e0a9-e50e24dcca9e': { // RX Characteristic
+           writable: true,
+           writableWithoutResponse: true, // Write No Response
+           onWrite: function(evt) {
+             console.log("RX data received:", evt.data);
+             // Elabora i dati ricevuti qui
+           }
+         },
+         '6e400003-b5a3-f393-e0a9-e50e24dcca9e': { // TX Characteristic
+           notify: true,
+           readable: true,
+           value: [0] // Inizialmente impostato a [0], modificabile in base alle esigenze
+         }
+       }
+    }, { uart: false });
+  }
+
+  // ----------------------------
+  // Funzione di aggiornamento dati BLE
+  // ----------------------------
+  function updateBLEData(hrm, bar, mag, gps, acc) {
+    const services = {
+      0x180D: { // Heart Rate
+        0x2A37: {
+          value: hrm && hrm.confidence >= 50 ? [0x06, hrm.bpm] : [0x06, 0],
+          notify: true
+        }
+      },
+      0x181A: {
+        0x2A6C: {
+          value: bar ? toByteArray(Math.round(bar.altitude * 100), 3, true) : [0, 0, 0],
+          notify: true
+        },
+        0x2A6D: {
+          value: bar ? toByteArray(Math.round(bar.pressure * 10), 4, false) : [0, 0, 0, 0],
+          notify: true
+        },
+        0x2A1F: {
+          value: bar ? toByteArray(Math.round(bar.temperature * 10), 2, true) : [0, 0],
+          notify: true
+        },
+        0x2AA1: {
+          value: mag ? encodeMag(mag) : [0, 0, 0, 0, 0, 0],
+          notify: true
+        },
+        // ----------------------------
+        // AGGIUNTA: Accelerometro
+        // ----------------------------
+        0x2ACC: {
+          value: acc ? encodeAccel(acc) : [0,0, 0,0, 0,0],
+          notify: true
+        }
+      },
+      0x1819: {
+        0x2A67: {
+          value: gps ? encodeGps(gps) : [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+          notify: true
+        }
+      }
+    };
+
     try {
-      NRF.updateServices(customServices);
+      NRF.updateServices(services);
     } catch (error) {
-      console.error("Errore nell'aggiornamento dei servizi BLE:", error);
       if (error.message.includes("BLE restart")) {
         NRF.disconnect();
-      } else {
-        NRF.setServices(customServices, { uart: false });
+      } else if (error.message.includes("UUID")) {
+        setupBLEAdvertising();
       }
     }
   }
 
-  // Funzione per codificare i dati dell'accelerometro
-  function encodeAccel(data) {
-    const x = toByteArray(Math.round(data.x * 1000), 2, true);
-    const y = toByteArray(Math.round(data.y * 1000), 2, true);
-    const z = toByteArray(Math.round(data.z * 1000), 2, true);
-    return [x[0], x[1], y[0], y[1], z[0], z[1]];
-  }
-
-  // Helper per convertire i valori in array di byte
+  // ----------------------------
+  // Helpers
+  // ----------------------------
   function toByteArray(value, numberOfBytes, isSigned) {
     let byteArray = new Array(numberOfBytes);
-    if (isSigned && value < 0) value += 1 << (numberOfBytes * 8);
+    if (isSigned && (value < 0)) value += 1 << (numberOfBytes * 8);
     for (let index = 0; index < numberOfBytes; index++) {
       byteArray[index] = (value >> (index * 8)) & 0xff;
     }
     return byteArray;
   }
 
-  // Avvio dei sensori
-  let hrmData, barData, accelData;
+  function encodeGps(data) {
+    const speed = toByteArray(Math.round(1000 * data.speed / 36), 2, false);
+    const lat = toByteArray(Math.round(data.lat * 10000000), 4, true);
+    const lon = toByteArray(Math.round(data.lon * 10000000), 4, true);
+    const elevation = toByteArray(Math.round(data.alt * 100), 3, true);
+    const heading = toByteArray(Math.round(data.course * 100), 2, false);
+    return [
+      157, 2,
+      speed[0], speed[1],
+      lat[0], lat[1], lat[2], lat[3],
+      lon[0], lon[1], lon[2], lon[3],
+      elevation[0], elevation[1], elevation[2],
+      heading[0], heading[1]
+    ];
+  }
+
+  function encodeMag(data) {
+    const x = toByteArray(Math.round(data.x), 2, true);
+    const y = toByteArray(Math.round(data.y), 2, true);
+    const z = toByteArray(Math.round(data.z), 2, true);
+    return [x[0], x[1], y[0], y[1], z[0], z[1]];
+  }
+
+  // ----------------------------
+  // Funzione per codificare l'accelerometro in 6 byte
+  // (x, y, z a 2 byte ciascuno, segno incluso)
+  // ----------------------------
+  function encodeAccel(data) {
+    const x = toByteArray(Math.round(data.x*1000), 2, true);
+    const y = toByteArray(Math.round(data.y*1000), 2, true);
+    const z = toByteArray(Math.round(data.z*1000), 2, true);
+    return [x[0], x[1], y[0], y[1], z[0], z[1]];
+  }
+
+  // Avvio dei servizi BLE
+  setupBLEAdvertising();
+
+  // Variabili globali che memorizzano gli ultimi dati ricevuti dai sensori
+  let hrm_1, bar_1, mag_1, gps_1, acc_1;
+
+  // ----------------------------
+  // Eventi dei sensori
+  // ----------------------------
   Bangle.on("HRM", (hrm) => {
-    hrmData = hrm;
-    updateCustomServices();
-  });
-  Bangle.on("pressure", (bar) => {
-    barData = bar;
-    updateCustomServices();
-  });
-  Bangle.on("accel", (accel) => {
-    accelData = accel;
-    updateCustomServices();
+    hrm_1 = hrm;
+    updateBLEData(hrm_1, bar_1, mag_1, gps_1, acc_1);
   });
 
-  Bangle.setHRMPower(true, "custom");
-  Bangle.setBarometerPower(true, "custom");
-  Bangle.setAccelerometerPower(true, "custom");
+  Bangle.on("pressure", (newBar) => {
+    bar_1 = newBar;
+    updateBLEData(hrm_1, bar_1, mag_1, gps_1, acc_1);
+  });
 
-  // Inizializzazione dei servizi BLE
-  updateCustomServices();
+  Bangle.on("mag", (newMag) => {
+    mag_1 = newMag;
+    updateBLEData(hrm_1, bar_1, mag_1, gps_1, acc_1);
+  });
+
+  Bangle.on("GPS", (newGps) => {
+    gps_1 = newGps;
+    updateBLEData(hrm_1, bar_1, mag_1, gps_1, acc_1);
+  });
+
+  // ----------------------------
+  // AGGIUNTA: Event listener per l'accelerometro
+  // ----------------------------
+  Bangle.on("accel", (newAcc) => {
+    acc_1 = newAcc;
+    updateBLEData(hrm_1, bar_1, mag_1, gps_1, acc_1);
+  });
+
+  // ----------------------------
+  // Attivazione dei sensori
+  // ----------------------------
+  Bangle.setHRMPower(true, "btadv");
+  Bangle.setBarometerPower(true, "btadv");
+  Bangle.setCompassPower(true, "btadv");
+  Bangle.setGPSPower(true, "btadv");
+
+  // L'accelerometro è solitamente già attivo di default;
+  // se necessario, puoi impostare un polling interval:
+  // Bangle.setPollInterval(80); // ms
 }
